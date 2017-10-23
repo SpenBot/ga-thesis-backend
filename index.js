bc4dc83495372f93d84382cad8e6dfd73e889cdc
Spenser Holstein <spenser.holstein@gmail.com>
2017-10-22 20:03:06 -0400

    inital messages bug fixed

--- index.js
+++ index.js
@@ -11,9 +11,28 @@ server.listen(4000, () => {
     console.log("\n\tServer active. Listening on port 4000\n")
 })
 
+// Schema & Model
+const Schema = require('./db/schema.js')
+const Message = Schema.Message
+
+
+
+
 // Socket.io Connection
 io.on('connection', (socket) => {
-  console.log('user connected')
-  socket.on('chat message', (msg) => io.emit('chat message', msg))
-  socket.on('disconnect', () => console.log('user disconnected'))
+
+  console.log('\n\tUser Connected')
+
+  Message.find({}).then(response => {
+    const messages = response.map(message => message.body)
+    io.emit('initial messages', messages)
+    console.log(Messages = `${messages}`)
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+    Message.create({body: msg})
+  })
+
+  socket.on('disconnect', () => console.log('\n\tUser Disconnected'))
 })
